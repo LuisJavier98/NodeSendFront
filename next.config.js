4df465bbad148ec7832fbbc0ffdf6cd6e7d610cc
Luis <luisjavier_2705@hotmail.com>
2023-03-14 09:45:47 -0500

    correccion de pass

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    backendURL: 'http://localhost:4000',
-    frontendURL: 'http://localhost:3000'
+    backendURL: 'http://nodesendback-production.up.railway.app',
+    frontendURL: 'https://joyful-bonbon-5a23d4.netlify.app'
   }
 }
 
