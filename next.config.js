25bb5711d7c310fbe3d2c44298c182cca418cf83
Luis <luisjavier_2705@hotmail.com>
2023-03-14 09:50:45 -0500

    nuevo https

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    backendURL: 'http://nodesendback-production.up.railway.app',
+    backendURL: 'https://nodesendback-production.up.railway.app',
     frontendURL: 'https://joyful-bonbon-5a23d4.netlify.app'
   }
 }
